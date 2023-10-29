54d956ccfa182a5143d07214a02b5e2977bb4576
Ankur Singh Bisht <ankursinghbisht007@gmail.com>
2023-10-29 21:40:10 +0530

    Right view of a binary tree (C++)

---
+++ C++/Right_view_of_tree.cpp
@@ -0,0 +1,171 @@
+/*
+Idea:
+iterating to the right most part of the tree , and if we enounter first node at a particular height, insert in the vector.
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// } Driver Code Ends
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+// Should return  right view of tree
+class Solution
+{
+public:
+    int findHeight(struct Node* node)
+    {
+        if (!node)
+            return 0; // returning 0 if node is null
+
+        return max(findHeight(node->right), findHeight(node->left)) + 1;
+        // returning height of node as max of heights of its child +1 (includes itself as well)
+    }
+
+    //Function to return a list containing elements of right view of the binary tree.
+    void solve(Node* root, int height, vector<int>& max_height, vector<int>& ans)
+    {
+        if (!root)
+            return;
+        if (height >= max_height.size())  // if at a  height, no node is encountered, this must be the right most node
+        {
+            ans.push_back(root->data);
+            max_height.push_back(height);
+        }
+
+        solve(root->right, height + 1, max_height, ans); // calling right part to get to right most node first
+        solve(root->left, height + 1, max_height, ans);
+    }
+    vector<int> rightView(Node* root)
+    {
+        // Your Code here
+        vector<int> ans, max_height;
+        solve(root, 0, max_height, ans);
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+int main() {
+    int t;
+    string  tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--)
+    {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        Solution ob;
+        vector<int> vec = ob.rightView(root);
+        for (int x : vec) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
